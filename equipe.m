d5f4ac60bc733386a7509d868ef5e918598407d2
Interface <gpinterface@gmail.com>
2014-12-09 14:49:39 -0200

    Create equipe.m

---
+++ equipe.m
@@ -0,0 +1,31 @@
+---
+layout: page
+permalink: /equipe/index.html
+title: Equipe
+tags: [Interface, Equipe, Pesquisa, Filosofia, Cultura]
+imagefeature: fourseasons.jpg
+chart: true
+---
+
+###Pesquisadores:
+     
+ - Cristiane Herres Terraza - [Currículo Lattes](http://lattes.cnpq.br/0750231114678276)
+ - Juliana Rocha de Faria Silva - [Currículo Lattes](http://lattes.cnpq.br/9756128301689917)
+ - Lusifátima Maria Gadêlha de Oliveira - [Currículo Lattes](http://lattes.cnpq.br/3255048615143056 )
+ - Marcos Ramon Gomes Ferreira - [Currículo Lattes](http://lattes.cnpq.br/9538072103558772)
+     
+###Estudantes:
+     
+- Ana Carolina de Albuquerque Costa
+- Clara Sales Viarum Brito (bolsista IFB/CNPq) - [Currículo Lattes](http://lattes.cnpq.br/0540135050283851)
+- Frank Wyllys Cabral Lira
+- Helena Medeiros Costa
+- Helrison Pereira
+- Jailson Rolim Ribeiro
+- Laura Fonseca Tonini
+- Layla Vanessa Veiga Tavares de Arruda
+- Lícia Nunes de Oliveira
+- Marianne Helise Bezerra Alvim
+- Paula Alessandra Silva Abreu
+- Rafael Alves De Assunção Oliveira
+- Yanni Pereira de Araújo Rodrigues
